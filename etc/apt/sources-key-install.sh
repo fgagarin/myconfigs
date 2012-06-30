f522737ad275cfdcee82e809c597f387a9d6bfe9
Fedor Gagarin <fgagarin@lysis.su>
2012-06-30 11:27:28 +0400

    Добавил источники приложеий Добавил настройку NetBeans

---
+++ etc/apt/sources-key-install.sh
@@ -0,0 +1,12 @@
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 614C4B38
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1378B444
+sudo apt-get update && sudo apt-get install medibuntu-keyring && sudo apt-get update 
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A69241F1
+sudo wget -O - http://apt.mucommander.com/apt.key | sudo apt-key add - 
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A1F196A8
+wget -q "http://deb.playonlinux.com/public.gpg" -O- | sudo apt-key add -
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0624A220
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F9CB8DB0
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys AF1CDFA9
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys 5044912E 
